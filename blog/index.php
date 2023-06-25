e2ecdd76e22d528b51381169167adf6e2f35636a
RezaAttarBeylondi <132735227+rezaattarbeylondi@users.noreply.github.com>
2023-06-26 00:04:18 +0330

    Add files via upload

---
+++ blog/index.php
@@ -0,0 +1,132 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>شال و روسری معجر</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <link rel="stylesheet" herf="css/bootstrap.css">
+  <link rel="stylesheet" herf="css/styl.css">
+
+</head>
+
+<body dir="rtl">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="https://eitaa.com/Majar_Queenly"><img src="image/logo majar.jpg" alt="لوگو معجر"
+        height="80px"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">صفحه اصلی<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            انواع شال
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">شال نخی</a>
+            <a class="dropdown-item" href="#">شال پشمی</a>
+            <a class="dropdown-item" href="#">شال ساتن</a>
+            <a class="dropdown-item" href="#">شال حریر</a>
+            <a class="dropdown-item" href="#">شال ترمه</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            انواع روسری
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">روسری نخی</a>
+            <a class="dropdown-item" href="#">روسری ابریشمی</a>
+            <a class="dropdown-item" href="#">روسری ترمه</a>
+            <a class="dropdown-item" href="#">روسری پشمی</a>
+            <a class="dropdown-item" href="#">روسری مخملی</a>
+            <a class="dropdown-item" href="#">روسری ساتن</a>
+            <a class="dropdown-item" href="#">روسری اسلپ</a>
+            <a class="dropdown-item" href="#">روسری وال اسلپ</a>
+            <a class="dropdown-item" href="#">روسری حریر</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            انواع ساق دست
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">ساق دست گیپور</a>
+            <a class="dropdown-item" href="#">ساق دست ساتن</a>
+            <a class="dropdown-item" href="#">ساق دست حریر</a>
+          </div>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ورود و عضویت</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">علاقه مندی ها</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">سبدخرید</a>
+        </li>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">راهنمای خرید </a>
+        </li>
+      </ul>
+    <div class=container>
+
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">جست وجو</button>
+         </form>
+       </div>
+      </nav>
+     <div class="row">
+         <div class="col-12 col-lg-6">
+           <div class="heed-image">
+             <img src="image/terme1.jpg" alt="" width="100%">
+           </div>
+        </div>
+        <div class="col-12 col-lg-3">
+           <div class="heed-image">
+              <img src="image/terme2.jpg" alt="" width="100%">
+           </div>
+         </div>
+        <div class="col-12 col-lg-3">
+          <div class="heed-image">
+              <div class="heed-image">
+              <img src="image/terme3.jpg" alt="" width="100%">
+             </div>
+           </div>
+          
+      </div>
+      <div class="content-text">
+        <span> 🌸 !برای ورود و دیدن محصولات بر روی لوگو بالای وبلاگ کلید کنید واز خرید خود لذت ببرید!🌸
+🌸الهی به امید تو🌸
+
+🧕🏻معجَر👑سرانداز باوقار😌
+
+🌿🌸زیبایی را در ساده پوشی تجربه کنید🌸🌿
+</span>
+      </div>
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+  integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</html>
